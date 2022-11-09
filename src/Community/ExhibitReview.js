d4ad9a00d8b49166bdb0f80016ea19e0c17dc6d1
Sangjun Park <sangjun1997@naver.com>
2022-11-10 00:50:07 +0900

    feat: Add page structure setting

--- src/Community/ExhibitReview.js
+++ src/Community/ExhibitReview.js
@@ -1,5 +1,9 @@
 let ExhibitReview = () => {
-
+    return (
+        <div>
+            커뮤니티 메인 페이지
+        </div>
+    )
 }
 
 export default ExhibitReview;
---
+++ src/ExhibitInfo/ExhibitInfo.js
@@ -0,0 +1,9 @@
+let ExhibitInfo = () => {
+    return (
+        <div>
+            전시회 정보 페이지
+        </div>
+    )
+}
+
+export default ExhibitInfo;
