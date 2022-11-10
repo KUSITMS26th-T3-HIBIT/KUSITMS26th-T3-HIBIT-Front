b5d86322d2e9bb77d722182003951d0b02f96e19
jake0319 <shjun1411@naver.com>
2022-11-10 19:18:28 +0900

    Test: code 이상 없음

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Global/Navbar/Navbar';
-
+import Main from './Global/Main/Main';
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Main></Main>
     </div>
   );
 }
