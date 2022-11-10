6d8f07c0b51d40ff5eb8c4e2a71426c31fe1057f
Sangjun Park <sangjun1997@naver.com>
2022-11-11 00:37:42 +0900

    Fix: set Nav bar position - MatchMain & Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Global/Navbar/Navbar';
+// import Navbar from './Global/Navbar/Navbar';
 import Main from './Global/Main/Main';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import ServiceInfo from './ServiceInfo/ServiceInfo';
+import MatchingMain from './Matching/MatchingMain';
+import ExhibitReview from './Community/ExhibitReview';
+import ExhibitInfo from './ExhibitInfo/ExhibitInfo';
+import Mypage from './Global/User/Mypage/Mypage';
+
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Main></Main>
+      <div className="Navbar">
+        <div className='NavContainer'>
+          <div className='NavLeft'>
+            <img className='Hibit_logo' width='156' height='34' src='/hibit_logo.png' onClick={() => { navigate('/') }} />
+            <div className='NavLeft_menu'>
+              <div className='NavItem' onClick={() => { navigate('/service') }}>서비스 소개</div>
+              <div className='NavItem' onClick={() => { navigate('/match') }}>매칭</div>
+              <div className='NavItem' onClick={() => { navigate('/community') }}>커뮤니티</div>
+              <div className='NavItem' onClick={() => { navigate('/exhibitinfo') }}>전시회 정보</div>
+            </div>
+          </div>
+          <div className='NavRight'>
+            <div className='NavMypage' onClick={() => { navigate('/mypage') }}>마이페이지</div>
+            <hr className='NavVerticalLine' />
+            <div className='NavRight_icon'>
+              <img className='ShareIcon' width='28' height='34' src='/share.png' onClick={() => { }} />
+              <img className='ProfileIcon' width='44' height='44' src='/profileImg.png' onClick={() => { }} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Routes>
+          <Route path='/' element={<Main/>} />
+          <Route path='/service' element={<ServiceInfo />} />
+          <Route path='/match' element={<MatchingMain />} />
+          <Route path='/community' element={<ExhibitReview />} />
+          <Route path='/exhibitinfo' element={<ExhibitInfo />} />
+
+          <Route path='/mypage' element={<Mypage />} />
+      </Routes>
+
+
+      {/* <Main></Main> */}
     </div>
   );
 }
--- src/Global/Slider/Slider.js
+++ src/Global/Slider/Slider.js
@@ -1,5 +1,29 @@
-let Slider = () => {
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
+let Slider = () => {
+    const responsive = {
+        superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+        desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+        tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+        mobile: { breakpoint: { max: 464, min: 0 }, items: 1}
+    };
+
+    return (
+        <div className='Slider'>
+            <div width='1920' height='600'>
+                <Carousel responsive={responsive}>
+                    <img width='1920' height='600' src='/slideimg1.jpg' />
+                    <img width='1920' height='600' src='/slideimg2.jpg' />
+                    <img width='1920' height='600' src='/slideimg3.jpg' />
+                    <img width='1920' height='600' src='/slideimg4.jpg' />
+                    <img width='1920' height='600' src='/slideimg5.jpg' />
+                </Carousel>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Slider;
