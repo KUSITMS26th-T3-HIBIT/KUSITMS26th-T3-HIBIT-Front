163cb19df0d718f32f91748185a0ff10a4b59d7e
Sangjun Park <sangjun1997@naver.com>
2022-11-24 03:23:47 +0900

    feat: 매칭 메인 페이지 UI 및 기능 완료

--- src/Matching/Match/CreateMatch.js
+++
@@ -1,9 +0,0 @@
-let CreateMatch = () => {
-    return (
-        <div>
-            매칭 게시글 발행 페이지
-        </div>
-    )
-}
-
-export default CreateMatch;
--- src/Matching/Match/MatchTab1.js
+++ src/Matching/Match/MatchTab1.js
@@ -1,7 +1,4 @@
-import axios from "axios";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import exhibit_category_data from "../../Global/Data/exhibit_category_data";
 
 const MatchTab1 = ({posts}) => {
     let navigate = useNavigate();
@@ -22,67 +19,31 @@ const MatchTab1 = ({posts}) => {
             {
                 posts.map((d, i)=>{
                     return (
-                        <div className="MatchTab-item">
-                            <div className="MatchTab-item-tags">
-                                {/* <div className="MatchTab-item-tag-category">{exhibit_category_data.id === +d.category}</div> */}
-                                <div className="MatchTab-item-tag-style">{"조용한 스타일"}</div>
+                        <div 
+                            className="MatchTab-item"
+                            onClick={()=>{
+                                navigate(`/match/detail/${posts[i].idx}`);
+                            }}
+                        >
+                            <img
+                                className="MatchTab-item-img" 
+                                src={`/exhibit${i%4 + 1}.png`}
+                            />
+                            <div className="MatchTab-item-content">
+                                <div className="MatchTab-item-tags">
+                                    <div className="MatchTab-item-tag-place">{d.area}</div>
+                                    <div className="MatchTab-item-tag-style">{"조용한 스타일"}</div>
+                                </div>
+                                <div className="MatchTab-item-exhibit-title">{d.exhibition}</div>
+                                <div className="MatchTab-item-exhibit-content">{d.content}</div>
                             </div>
-                            <div className="MatchTab-item-exhibit-title">{d.exhibition}</div>
-                            <div className="MatchTab-item-title">{d.content}</div>
                         </div>
                     )
                 })
             }
 
 
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"장 줄리앙 : 그러면 거기"}</div>
-                <div className="MatchTab-item-title">{"함께 가실 분 구합니다!"}</div>
-            </div>
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"로그아웃"}</div>
-                <div className="MatchTab-item-title">{"오늘 하루 쉬어가고 싶으신 분?"}</div>
-            </div>
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"하리보 골드베렌 100주년 생일 기념전"}</div>
-                <div className="MatchTab-item-title">{"달콤한 시간 보내실 분!"}</div>
-            </div>
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"팀 버튼 특별전"}</div>
-                <div className="MatchTab-item-title">{"안 보면 손해!"}</div>
-            </div>
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"프랑코 폰타나 : 컬러 인 라이프"}</div>
-                <div className="MatchTab-item-title">{"다채로운 시간을 만들어봐요."}</div>
-            </div>
-            <div className="MatchTab-item">
-                <div className="MatchTab-item-tags">
-                    <div className="MatchTab-item-tag-category">{"전시 카테고리"}</div>
-                    <div className="MatchTab-item-tag-style">{"회원가입 시 관람 스타일"}</div>
-                </div>
-                <div className="MatchTab-item-exhibit-title">{"맥스 달튼, 영화의 순간들"}</div>
-                <div className="MatchTab-item-title">{"영화 속 순간으로."}</div>
-            </div>
+            
             
         </div>
     )
