fc1f1414ec67e0f6fc236f3eae9baff28be3e761
Sangjun Park <sangjun1997@naver.com>
2022-11-25 03:38:45 +0900

    feat: 알림 모달창 UI

--- src/Global/Nav/UserGreeting.js
+++ src/Global/Nav/UserGreeting.js
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/token";
+import Modal from "react-modal";
 
 const UserGreeting = () => {
     let navigate = useNavigate();
     let dispatch = useDispatch();
     axios.defaults.headers.common['Authorization'] = `${localStorage.getItem('accessToken')}`
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
 
     return (
@@ -31,7 +34,36 @@ const UserGreeting = () => {
                     <div className="nav-user-right-components">
                         <div className='nav-user-mypage' onClick={() => { navigate('/mypage') }}>마이페이지</div>
                         <hr className='nav-user-vertical-line' />
-                        <img className='nav-user-profile-icon' src='/profile_frame.png' onClick={() => { }} />
+                        <img className='nav-user-profile-icon' src='/profile_frame.png' onClick={() => setModalIsOpen(true)} />
+                        <Modal 
+                            isOpen={modalIsOpen}
+                            onRequestClose={()=>setModalIsOpen(false)}
+                            style={{
+                                overlay: {
+                                    position: 'fixed',
+                                    top: 0,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    backgroundColor: '#5E1EC7',
+                                    opacity: 0.9
+                                  },
+                                  content: {
+                                    width: '300px',
+                                    height: '200px',
+                                    position: 'absolute',
+                                    top: '130px',
+                                    left: '1400px',
+                                    border: '1px solid #ccc',
+                                    background: '#fff',
+                                    overflow: 'auto',
+                                    WebkitOverflowScrolling: 'touch',
+                                    borderRadius: '4px',
+                                    outline: 'none',
+                                    padding: '20px'
+                                  }
+                            }}
+                        />
                         <img
                             className='nav-user-logout-icon'
                             src='/logout.png'
